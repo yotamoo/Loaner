07dfc79683e5060a7c750d81a6d44a94fef36a4c
Yotam Ohayon <yotam.ohayon@gorillasapp.com>
2022-12-14 22:34:41 +0100

    deploy script

---
+++ deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'survey museum census believe glory artefact canvas aisle that question always fish',
+  'https://goerli.infura.io/v3/cf0c0d46e4004c5dad9b277e3da56d5a'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();

bb24c59be91c69fc34f931771ea066dbf0df8ca6
Yotam Ohayon <yotam.ohayon@gorillasapp.com>
2022-12-15 17:34:11 +0100

    added provider

--- scripts/compile.js
+++ scripts/compile.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
 
-const contractPath = path.resolve(__dirname, 'contracts', 'Loaner.sol');
+const contractPath = path.resolve(__dirname, '../contracts', 'Loaner.sol');
 const source = fs.readFileSync(contractPath, 'utf8');
 
 module.exports = solc.compile(source, 1).contracts[':Loaner'];
